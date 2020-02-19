1882bd16bbdf2fe0bbd5bc16d5eeb03045e1345b
Daniele Pira <pira.daniela@gmail.com>
2020-02-19 16:59:39 +0100

    Improve day01.cpp solution

--- 2019/cpp/day01.cpp
+++ 2019/cpp/day01.cpp
@@ -1,44 +1,28 @@
-#include <vector>
-#include <string>
 #include <iostream>
 #include <stdexcept>
 #include <fstream>
-#include <algorithm>
 #include <numeric>
 #include <iterator>
 
 using namespace std;
 
-vector<int> load_masses(const string& filename)
-{
-    ifstream file {filename};
-
-    if(!file.is_open()) throw runtime_error {"error while opening file"};
-
-    vector<int> masses {istream_iterator<int> {file}, istream_iterator<int> {}};
-
-    if(file.bad()) throw runtime_error {"error while reading file"};
-
-    return masses;
-}
-
 int compute_fuel(const int mass)
 {
-    int fuel {mass / 3 - 2};
+    int fuel{ mass / 3 - 2 };
     return fuel > 0 ? fuel + compute_fuel(fuel) : 0;
 }
 
-int compute_total_fuel(vector<int>& masses)
+int main(const int argc, char const* const argv[])
 {
-    transform(masses.begin(), masses.end(), masses.begin(), compute_fuel);
-    return accumulate(masses.begin(), masses.end(), 0);
-}
+    if (argc != 2) throw invalid_argument{ "missing input filename" };
 
-int main(const int argc, char const * const argv[])
-{
-    if(argc != 2) throw invalid_argument {"missing input filename"};
+    ifstream file{ argv[1] };
 
-    vector<int> masses {load_masses(argv[1])};
+    if (!file.is_open()) throw runtime_error{ "error while opening file" };
 
-    cout << compute_total_fuel(masses) << endl;
+    int fuel = transform_reduce(istream_iterator<int> {file}, istream_iterator<int> {}, 0, plus<>(), compute_fuel);
+
+    if (file.bad()) throw runtime_error{ "error while reading file" };
+
+    cout << fuel << endl;
 }
