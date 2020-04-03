15ad3806ff2afd5b1146e9c2031bf442cb553813
Daniele Pira <pira.daniele@gmail.com>
2020-04-03 10:15:22 +0200

    Update day01 Rust implementation

--- 2019/rust/day01.rs
+++ 2019/rust/day01.rs
@@ -1,24 +1,26 @@
-use std::error::Error;
 use std::env::args_os;
 use std::fs::read_to_string;
 use std::iter::successors;
 
-fn checked_compute_fuel(&mass: &i32) -> Option<i32>
+fn checked_compute_fuel(mass: i32) -> Option<i32>
 {
     if mass >= 9 {Some(mass / 3 - 2)} else {None}
 }
 
-fn recursive_compute_fuel(mass: &i32) -> i32
+fn recursive_compute_fuel(mass: i32) -> i32
 {
-    successors(checked_compute_fuel(mass), checked_compute_fuel).sum()
+    successors(checked_compute_fuel(mass),|&m| checked_compute_fuel(m)).sum()
 }
 
-fn main() -> Result<(), Box<dyn Error>>
+fn parse_mass(line: &str) -> i32
 {
-    let pathname         = args_os().nth(1).ok_or("Missing input pathname")?;
-    let content          = read_to_string(pathname)?;
-    let masses: Vec<i32> = content.lines().map(|line| line.parse()).collect::<Result<_, _>>()?;
-    let fuel: i32        = masses.iter().map(recursive_compute_fuel).sum();
+    line.parse().expect("Error while parsing file")
+}
+
+fn main()
+{
+    let pathname  = args_os().nth(1).expect("Missing input pathname");
+    let content   = read_to_string(pathname).expect("Error while reading file");
+    let fuel: i32 = content.lines().map(parse_mass).map(recursive_compute_fuel).sum();
     println!("{}", fuel);
-    Ok(())
 }
